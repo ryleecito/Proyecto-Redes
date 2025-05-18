e849c23e747916327f06a71814b1c00f55e1afbd
Administrator <manuel.dealmada.rodriguez@est.una.ac.cr>
2025-05-17 19:46:15 -0600

    Probando Pipeline

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-return '¡Hola desde Flask en Docker en Azure - versión 2!'
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return 'Hola desde Flask, Docker en Azure '
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
